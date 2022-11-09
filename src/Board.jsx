9baf272de8a91d6bfd9864c080a66e736a54a0d8
endo1322 <endo.yuto.19@shizuoka.ac.jp>
2022-11-09 19:06:47 +0900

    変更前コミット

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,59 +1,31 @@
 import React, { useState, memo } from 'react'
 import Square from "./Square"
-import calculateWinner from "./calculateWinner"
 import { useCallback } from 'react';
+import calculateWinner from "./calculateWinner"
 
-const Board = memo(() => {
-  const nullList = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ];
-  const [status, setStatus] = useState('Next player: X')
-  const [squares, setSquares] = useState(nullList)
-  const [xIsNext, setXIsNext] = useState(true)
-  // const [win, setWin] = useState(false)
-  
-  const hundleClick = (i, winner) => {
-    // .slice()だとうまくいかない
-    const new_squares = [...squares]
-    setSquares(new_squares)
-    console.log(squares)
-    if (winner) {
-      setStatus('Winner: ' + winner)
-    } else {
-      setStatus('Next player: ' + (xIsNext ? 'X' : 'O'))
-    }
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const state = xIsNext ? 'X' : 'O';
-    setSquares(
-      squares.map((value, index) => (index === i ? state : value))
-    )
-    setXIsNext(!xIsNext)
-
-  }
+const Board = memo((props) => {
 
   const renderSquare = useCallback((i) => {
-    const winner = calculateWinner(squares);
+    // const winner = calculateWinner(props.squares[0].Square);
+    const winner = 'x'
     return (
+      // <Square
+      //   value={props.squares[0].Square[i]}
+      //   onClick={() => props.onClick(i, winner)}
+      // />
       <Square
-        value={squares[i]}
-        onClick={() => hundleClick(i, winner)}
-      />
+      value={props.squares[0].square[i]}
+      onClick={() => props.onClick(i, winner)}
+    />
+      // <Square
+      //   value={props.squares[0].square[i]}
+      // />
     );
   })
 
   return (
     <div>
-          <div className="mb-2.5">{status}</div>
+          {/* <div className="mb-2.5">{status}</div> */}
           <div className="clear-both content-none">
             {renderSquare(0)}
             {renderSquare(1)}
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,18 +1,83 @@
 import React from 'react';
-// import { useState } from "react";
+import { useState } from "react";
 // import ReactDOM from 'react-dom/client';
 import './index.css'; 
 import Board from "./Board.jsx";
+import Square from './Square';
+import calculateWinner from "./calculateWinner"
+
 
 export const Game = () => {
+    const nullList = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ];
+    const [gameInfo, setGameInfo] = useState([
+        {
+            status: 'Next player: X',
+            square: nullList,
+            xIsNext: true
+        }
+    ])
+
+    const hundleClick = (i, winner) => {
+        // const new_status = null
+        // if (winner) {
+        //   new_status = ('Winner: ' + winner)
+        // } else {
+        //   new_status = ('Next player: ' + (gameInfo[0].xIsNext ? 'X' : 'O'))
+        // }
+        // if (calculateWinner(gameInfo[0].square) || gameInfo[0].square[i]) {
+        //   return;
+        // }
+        // const state = gameInfo[0].xIsNext ? 'X' : 'O';
+        // const new_square = gameInfo[0].square.map((value, index) => (index === i ? state : value))
+        // setGameInfo(
+        //     {
+        //         status: new_status,
+        //         square: new_square,
+        //         xIsNext: !gameInfo[0].xIsNext
+        //     }
+        // )
+        console.log('click')
+      }
+
+
+    
+
+    const unshift = () => {
+        gameInfo.unshift(
+            {
+                status: 'Next player: O',
+                square: nullList,
+                xIsNext: false
+            }
+        )
+        console.log(gameInfo)
+    }
+
+    const shift = () => {
+        const pull = gameInfo.shift()
+        console.log(pull)
+        console.log(gameInfo)
+    }
+
     return (
         <div className="flex-row text-center">
             <div className="game-board">
-            <Board />
+            <Board squares={gameInfo} onClick={() => hundleClick()} />
+            {/* <Board squares={gameInfo} /> */}
             </div>
             <div className="ml-5">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            {/* <div>{gameinfo.status}</div> */}
+            <ol>{/*TODO*/}</ol>
             </div>
         </div>
     );
